cf67ac9e7e05777f8989592aa19bf657a241bcaf
Natalie Biderman <nataliebiderman@Natalies-MacBook-Pro.local>
2022-07-15 11:55:25 -0400

    add saving function

--- Task/Tools/Functions.js
+++ Task/Tools/Functions.js
@@ -315,3 +315,24 @@ function sum(input){
      } // repeat_category_familiarity
     return repeat_trial
    }
+
+   // Save data to file functions
+   function save_server_data(name, data) {
+     var xhr = new XMLHttpRequest();
+     //xhr.addEventListener("load", onComplete);
+     xhr.open('POST', 'Tools/write_data.php'); // 'write_data.php' is the path to the php file described above.
+     xhr.setRequestHeader('Content-Type', 'application/json');
+     xhr.send(JSON.stringify({
+       filename: name,
+       filedata: data
+     }));
+   }
+
+   function save_local_data(name, data){
+      var a         = document.createElement('a');
+      a.href        = 'data:attachment/csv,' +  encodeURIComponent(data);
+      a.target      = '_blank';
+      a.download    = name;
+      document.body.appendChild(a);
+      a.click();
+   }
